36a413f3035590ba92d4d3efed883dfff52208f7
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-03-04 11:22:48 -0500

    Agregado funcion de salario

--- Ejercicio-3-18/src/ejercicio/pkg3/pkg18/Ejercicio318.java
+++ Ejercicio-3-18/src/ejercicio/pkg3/pkg18/Ejercicio318.java
@@ -15,9 +15,19 @@ public class Ejercicio318 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("tnjg4rymh,!");
+        System.out.println("Nothing wrong with me");
         int x = 10;
+        
         System.out.println("por cada "+x+" estudiantes de POO, "+x/2+" son la mitad de ellos");
     }
+    static double[] datos_salario_semanal( double salario_hora, int horas){
+        double retencion_fuente = 0.125;
+        double datos[] = new double[3];
+        
+        datos[0] = salario_hora * horas;
+        datos[1] = retencion_fuente * datos[0];
+        datos[2] = datos[0]- datos[1];
     
+        return datos;
+    }
 }
