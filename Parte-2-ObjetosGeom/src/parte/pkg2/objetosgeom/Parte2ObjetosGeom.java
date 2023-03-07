2dc48f4004e061882312e03e546db02155b005f0
Jhojan Salum <jhojanjja@gmail.com>
2023-03-06 21:31:39 -0500

    Añadir subclase Rectángulo y su testeo al Main

--- Parte-2-ObjetosGeom/src/parte/pkg2/objetosgeom/Parte2ObjetosGeom.java
+++ Parte-2-ObjetosGeom/src/parte/pkg2/objetosgeom/Parte2ObjetosGeom.java
@@ -15,6 +15,15 @@ public class Parte2ObjetosGeom {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Testing the Subclass Rectangulo
+        Rectangulo r1 = new Rectangulo(2, 3);
+        System.out.println("Rectangulo");
+        System.out.println("Base: " + r1.base);
+        System.out.println("Altura: " + r1.altura);
+        System.out.println("Area: " + r1.area());
+        System.out.println("Perimetro: " + r1.perimeter());
+
+
     }
     
 }
