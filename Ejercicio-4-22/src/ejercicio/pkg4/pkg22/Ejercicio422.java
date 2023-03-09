9cf9762a6789f5a1cb4ee868e48292980b4aba70
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-03-08 22:30:14 -0500

    Terminado el ejercicio 4-22, solo se crean polinomios de grado 2

--- Ejercicio-4-22/src/ejercicio/pkg4/pkg22/Ejercicio422.java
+++ Ejercicio-4-22/src/ejercicio/pkg4/pkg22/Ejercicio422.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio.pkg4.pkg22;
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +12,26 @@ package ejercicio.pkg4.pkg22;
 public class Ejercicio422 {
 
     public static void main(String[] args) {
-        //Testeo de la clase Polinomio
 
-        double[] coeficientes = {1, 2, 3};
-        Polinomio polinomio = new Polinomio(2, coeficientes);
+        Scanner entrada = new Scanner(System.in);
+        double[] coeficientes = new double[3];
+        //Lectura de los coeficientes por parte del usuario
+
+        System.out.println("Se creara un polinomio de grado 2");
+        System.out.println("Sus coeficientes se leen de izquierda a derecha, y su orden es descendente");
+
+        for(int i = 0; i < coeficientes.length; i++){
+            System.out.println("Ingrese el coeficiente " + (i + 1));
+            coeficientes[i] = entrada.nextDouble();
+        }
+
+        //Testeo de la clase Polinoimio
+        Grado2 polinomio = new Grado2(2, coeficientes);
         System.out.println("El polinomio es: " + polinomio.coeficientes[0] + "x^2 + " + polinomio.coeficientes[1] + "x + " + polinomio.coeficientes[2]);
+        polinomio.calcular_raices();
+
+        System.out.println("Las raices son: ");
+        polinomio.mostrar_raices();
        
     }
     
---
+++ Ejercicio-4-22/src/ejercicio/pkg4/pkg22/Grado2.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio.pkg4.pkg22;
+
+/**
+ *
+ * @author kkmie
+ */
+public class Grado2 extends Polinomio{
+
+    /*Esta clase es por "temporal"
+    Se espera encontrar una generalizacion para sus metodos
+    cuando se tienen polinomios de grado n
+    probablemente hasta para variable compleja*/
+        
+    public Grado2(int grado, double[] coeficientes) {
+        super(grado, coeficientes);
+    }
+    
+    /* Metodo tradicional por condicionales
+    notar que los valores se guardan en los atributos del objeto*/
+    public void calcular_raices(){
+        double a = this.coeficientes[0];
+        double b = this.coeficientes[1];
+        double c = this.coeficientes[2];
+        double discriminante = Math.pow(b, 2) - 4 * a * c;
+
+        if(discriminante > 0){
+            this.raices[0] = (-b + Math.sqrt(discriminante)) / (2 * a);
+            this.raices[1] = (-b - Math.sqrt(discriminante)) / (2 * a);
+        }else if(discriminante == 0){
+            this.raices[0] = -b / (2 * a);
+        }else{
+           for (int i = 0; i < this.raices.length; i++) {
+                this.raices[i] = Double.NaN;
+            }
+        }
+    }
+    
+    /* El metodo isNaN de la clase double, verifica si es nulo el valor
+    Retorna un booleano de falso o verdadero*/    
+    public void mostrar_raices(){
+        for (int i = 0; i < this.raices.length; i++) {
+            if (Double.isNaN(this.raices[i])) {
+                System.out.println("La raiz " + (i + 1) + " es compleja");
+            } else {
+                System.out.println("La raiz " + (i + 1) + " es: " + this.raices[i]);
+            }
+        }
+    }
+    
+}
