378263d8c6d5357bd2faf14d7e6eeaf987368df5
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-03-09 17:10:56 -0500

    Completado el ejercicio, se reciben los datos y se computa el algoritmo pedido

--- Ejercicio-4-23/src/ejercicio/pkg4/pkg23/Ejercicio422.java
+++ Ejercicio-4-23/src/ejercicio/pkg4/pkg23/Ejercicio422.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio.pkg4.pkg23;
+import java.util.Scanner;
 
 /**
  *
@@ -10,11 +11,26 @@ package ejercicio.pkg4.pkg23;
  */
 public class Ejercicio422 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Creamos la variables a utilizar
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        double horas_mensuales;
+        double salario_hora;
+
+        //Pedimos los datos al usuario
+        System.out.println("Ingrese el nombre del empleado: ");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese las horas mensuales trabajadas por el empleado: ");
+        horas_mensuales = entrada.nextInt();
+        System.out.println("Ingrese el salario por hora del empleado: ");
+        salario_hora = entrada.nextDouble();
+
+        //Creamos el objeto empleado
+        empleado empleado = new empleado(nombre, horas_mensuales, salario_hora);
+
+        //Procedemos a motrar los datos
+        empleado.mostrar_datos();
     }
     
 }
--- Ejercicio-4-23/src/ejercicio/pkg4/pkg23/empleado.java
+++ Ejercicio-4-23/src/ejercicio/pkg4/pkg23/empleado.java
@@ -13,21 +13,25 @@ public class empleado {
     /*Declaracion de atributos */
     
     private String nombre;
-    private int horas_mensuales;
+    private double horas_mensuales;
     private double salario_hora;
     
     /*Declaracion de metodos*/
 
-    public empleado(String nombre, int horas_mensuales, double salario_hora){
+    public empleado(String nombre, double horas_mensuales, double salario_hora){
         this.nombre = nombre;
         this.horas_mensuales = horas_mensuales;
         this.salario_hora = salario_hora;
     }
-
+    
+    public double calcular_sal_mensual(){
+        return this.horas_mensuales*this.salario_hora;
+    }
+    
     public void mostrar_datos(){
-       if (this.horas_mensuales*this.horas_mensuales > 450000){
+       if (calcular_sal_mensual() > 450000){
         System.out.println("Nombre: " + this.nombre);
-        System.out.println("Salario mensual: " + (this.salario_hora * this.horas_mensuales));
+        System.out.println("Salario mensual: " + this.calcular_sal_mensual());
        }else{
         System.out.println("Nombre: " + this.nombre);
        }
