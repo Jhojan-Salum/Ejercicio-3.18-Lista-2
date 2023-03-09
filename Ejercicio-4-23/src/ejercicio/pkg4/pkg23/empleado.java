d823925517f69d4af55eb87e63afd81c88731867
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-03-09 16:53:25 -0500

    Alterada la clase empleado con los metodos requeridos

--- Ejercicio-4-23/src/ejercicio/pkg4/pkg23/empleado.java
+++ Ejercicio-4-23/src/ejercicio/pkg4/pkg23/empleado.java
@@ -9,35 +9,27 @@ package ejercicio.pkg4.pkg23;
  * @author kkmie
  */
 public class empleado {
+
+    /*Declaracion de atributos */
     
-    private int codigo_empleado;
-    private String nombres;
-    private int horas_trabajadas;
+    private String nombre;
+    private int horas_mensuales;
     private double salario_hora;
-    private double retencion_fuente = 0.125;
-    private double salario_bruto;
-    private double salario_neto;
     
-    public empleado(int codigo_empleado, String nombres, int horas_trabajadas, double salario_hora){
-        this.codigo_empleado = codigo_empleado;
-        this.nombres = nombres;
-        this.horas_trabajadas = horas_trabajadas;
+    /*Declaracion de metodos*/
+
+    public empleado(String nombre, int horas_mensuales, double salario_hora){
+        this.nombre = nombre;
+        this.horas_mensuales = horas_mensuales;
         this.salario_hora = salario_hora;
     }
 
-    public void cambiar_retencion(double retencion_fuente){
-        this.retencion_fuente = retencion_fuente;
-    }
-    
-    public void calcular_salario(){
-        this.salario_bruto = this.salario_hora * this.horas_trabajadas;
-        this.salario_neto = this.salario_bruto - (this.salario_bruto * this.retencion_fuente);
-    }
-    
     public void mostrar_datos(){
-        System.out.println("Código del empleado: " + this.codigo_empleado);
-        System.out.println("Nombres: " + this.nombres);
-        System.out.println("Salario bruto: " + this.salario_bruto);
-        System.out.println("Salario neto: " + this.salario_neto);
+       if (this.horas_mensuales*this.horas_mensuales > 450000){
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Salario mensual: " + (this.salario_hora * this.horas_mensuales));
+       }else{
+        System.out.println("Nombre: " + this.nombre);
+       }
     }
 }
