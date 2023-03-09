5b5dd15d36bf495349f84bebe5c9d5b2853c059c
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-03-09 16:42:06 -0500

    Creada la clase empleado, pero sin la funcionalidad pedida

---
+++ Ejercicio-4-23/src/ejercicio/pkg4/pkg23/empleado.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio.pkg4.pkg23;
+
+/**
+ *
+ * @author kkmie
+ */
+public class empleado {
+    
+    private int codigo_empleado;
+    private String nombres;
+    private int horas_trabajadas;
+    private double salario_hora;
+    private double retencion_fuente = 0.125;
+    private double salario_bruto;
+    private double salario_neto;
+    
+    public empleado(int codigo_empleado, String nombres, int horas_trabajadas, double salario_hora){
+        this.codigo_empleado = codigo_empleado;
+        this.nombres = nombres;
+        this.horas_trabajadas = horas_trabajadas;
+        this.salario_hora = salario_hora;
+    }
+
+    public void cambiar_retencion(double retencion_fuente){
+        this.retencion_fuente = retencion_fuente;
+    }
+    
+    public void calcular_salario(){
+        this.salario_bruto = this.salario_hora * this.horas_trabajadas;
+        this.salario_neto = this.salario_bruto - (this.salario_bruto * this.retencion_fuente);
+    }
+    
+    public void mostrar_datos(){
+        System.out.println("Código del empleado: " + this.codigo_empleado);
+        System.out.println("Nombres: " + this.nombres);
+        System.out.println("Salario bruto: " + this.salario_bruto);
+        System.out.println("Salario neto: " + this.salario_neto);
+    }
+}
